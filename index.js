c45f053c0e042d96c34240f42a51570cefb5073e
muhammadsuleman2022skipq <muhammadsuleman.amjad.skipq@gmail.com>
2022-12-17 20:30:03 +0500

    skip ci

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'Hello Node! - Prod Env\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`TServer running on http://localhost:${port}/`);
+});
