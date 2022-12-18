8527489cc8b684f2cb960067c0e6b78a4ae160be
muhammadsuleman2022skipq <muhammadsuleman.amjad.skipq@gmail.com>
2022-12-18 17:08:33 +0500

    Stage

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! - Prod Env\n'
+  const msg = 'Hello Node! - Stage Env\n'
   res.end(msg);
 });
 
